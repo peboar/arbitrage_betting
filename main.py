753e66985ec7e80c8b1a19f72629f0347b86c4bd
per <perboekhout@hotmail.com>
2022-10-08 19:10:16 +0200

    Commit

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import json
-from bets import *
-from snabbare import snabbare
+
 # from bet365 import bet365
 # from leovegas import leovegas
 # from unibet import unibet
-from redbet import redbet
-from bethard import bethard
-from betfair import betfair
-from comeon import comeon
-from betsafe import betsafe
-from sport888 import sport888
-from hajper import hajper
 
 with open("/home/per/Desktop/pythonProject/arbitrage_betting/top_leagues.json") as file:
     data = json.load(file)
